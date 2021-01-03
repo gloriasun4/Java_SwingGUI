e1d0f09b7a3a6cb9eec702369456e998f4c8fb1e
Gloria Sun <gloriasun4@gmail.com>
2021-01-03 11:43:22 -0500

    JLabel pt. 1

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -6,16 +6,18 @@ public class Main {
 
     public static void main(String[] args) {
 
+        ImageIcon image = new ImageIcon("eggbacon.jpg");
+        JLabel label = new JLabel();
+        label.setText("Bro, do you even code?");
+        label.setIcon(image);
+        label.setHorizontalTextPosition(JLabel.CENTER);
+        label.setVerticalTextPosition(JLabel.TOP);
+
         JFrame frame = new JFrame();
-        frame.setTitle("My first GUI!");
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.setResizable(false);
-        frame.setSize(420,420);
+        frame.setSize(500,500);
         frame.setVisible(true);
-
-        ImageIcon image = new ImageIcon("eggbacon.png");
-        frame.setIconImage(image.getImage());
-
+        frame.add(label);
 
     }
 }
