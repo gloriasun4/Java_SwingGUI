f4a2ac720c00f6ca53153d8cac7aeb42b825fde8
Gloria Sun <53097343+gloriasun4@users.noreply.github.com>
2021-01-17 15:44:34 -0500

    Buttons & BorderLayout

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -8,39 +8,60 @@ public class Main {
 
     public static void main(String[] args) {
 
-        ImageIcon icon = new ImageIcon("egg_small.jpg");
-
-        JLabel label = new JLabel();
-        label.setText("Hi");
-        label.setIcon(icon);
-        //label.setVerticalAlignment(JLabel.BOTTOM);
-        //label.setHorizontalAlignment(JLabel.RIGHT);
-        label.setBounds(0,0,75,75);
-
-        JPanel redPanel = new JPanel();
-        redPanel.setBackground(Color.red);
-        redPanel.setBounds(0, 0, 250, 250);
-        redPanel.setLayout(null);
-
-        JPanel bluePanel = new JPanel();
-        bluePanel.setBackground(Color.blue);
-        bluePanel.setBounds(250, 0, 250, 250);
-        bluePanel.setLayout(null);
-
-        JPanel greenPanel = new JPanel();
-        greenPanel.setBackground(Color.green);
-        greenPanel.setBounds(0, 250, 500, 250);
-        greenPanel.setLayout(null);
-
         JFrame frame = new JFrame();
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.setLayout(null);
-        frame.setSize(750,750);
+        frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        frame.setSize(500,500);
+        frame.setLayout(new BorderLayout(10,10));
         frame.setVisible(true);
-        bluePanel.add(label);
-        frame.add(redPanel);
-        frame.add(bluePanel);
-        frame.add(greenPanel);
 
+        JPanel panel1 = new JPanel();
+        JPanel panel2 = new JPanel();
+        JPanel panel3 = new JPanel();
+        JPanel panel4 = new JPanel();
+        JPanel panel5 = new JPanel();
+
+        panel1.setBackground(Color.red);
+        panel2.setBackground(Color.green);
+        panel3.setBackground(Color.yellow);
+        panel4.setBackground(Color.magenta);
+        panel5.setBackground(Color.blue);
+
+        panel1.setPreferredSize(new Dimension(0,100));
+        panel2.setPreferredSize(new Dimension(100,0));
+        panel3.setPreferredSize(new Dimension(100,100));
+        panel4.setPreferredSize(new Dimension(100,100));
+        panel5.setPreferredSize(new Dimension(100,100));
+
+        JPanel panel6 = new JPanel();
+        JPanel panel7 = new JPanel();
+        JPanel panel8 = new JPanel();
+        JPanel panel9 = new JPanel();
+        JPanel panel10 = new JPanel();
+
+        panel6.setBackground(Color.black);
+        panel7.setBackground(Color.darkGray);
+        panel8.setBackground(Color.gray);
+        panel9.setBackground(Color.lightGray);
+        panel10.setBackground(Color.white);
+
+        panel5.setLayout(new BorderLayout(10,10));
+
+        panel6.setPreferredSize(new Dimension(0,50));
+        panel7.setPreferredSize(new Dimension(50,0));
+        panel8.setPreferredSize(new Dimension(50,50));
+        panel9.setPreferredSize(new Dimension(50,50));
+        panel10.setPreferredSize(new Dimension(50,50));
+
+        panel5.add(panel6, BorderLayout.NORTH);
+        panel5.add(panel7, BorderLayout.WEST);
+        panel5.add(panel8, BorderLayout.EAST);
+        panel5.add(panel9, BorderLayout.SOUTH);
+        panel5.add(panel10, BorderLayout.CENTER);
+
+        frame.add(panel1, BorderLayout.NORTH);
+        frame.add(panel2, BorderLayout.WEST);
+        frame.add(panel3, BorderLayout.EAST);
+        frame.add(panel4, BorderLayout.SOUTH);
+        frame.add(panel5, BorderLayout.CENTER);
     }
 }
--- src/com/company/MyFrame.java
+++ src/com/company/MyFrame.java
@@ -2,17 +2,55 @@ package com.company;
 
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
-public class MyFrame extends JFrame {
+public class MyFrame extends JFrame implements ActionListener {
+
+    JButton button;
+    JLabel label;
+
     MyFrame() {
-        this.setTitle("My first GUI!");
-        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        this.setResizable(false);
-        this.setSize(420, 420);
-        this.setVisible(true);
 
-        ImageIcon image = new ImageIcon("eggbacon.jpg");
-        this.setIconImage(image.getImage());
-        this.getContentPane().setBackground(new Color(123, 50, 250));
+        ImageIcon icon = new ImageIcon("egg_smaller.jpg");
+        ImageIcon icon2 = new ImageIcon("mr_small.gif");
+
+        button = new JButton();
+        label = new JLabel();
+
+        label.setIcon(icon2);
+        label.setBounds(150,250,150,150);
+        label.setVisible(false);
+
+        button.setBounds(200,200,250,100);
+        button.addActionListener(this);
+        button.setText("I'm a button. period");
+        button.setFocusable(false);
+        button.setIcon(icon);
+        button.setHorizontalTextPosition(JButton.CENTER);
+        button.setVerticalTextPosition(JButton.BOTTOM);
+        button.setIconTextGap(-10);
+        button.setFont(new Font("Comic Sans", Font.BOLD, 10));
+        button.setForeground(Color.cyan);
+        button.setBackground(Color.lightGray);
+        button.setBorder(BorderFactory.createEtchedBorder());
+
+        this.setDefaultCloseOperation(EXIT_ON_CLOSE);
+        this.setLayout(null);
+        this.setSize(500,500);
+
+        this.setVisible(true);
+        this.add(button);
+        this.add(label);
+
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        if(e.getSource()== button) {
+            //System.out.println("purr");
+           // button.setEnabled(false);
+            label.setVisible(true);
+        }
     }
 }
