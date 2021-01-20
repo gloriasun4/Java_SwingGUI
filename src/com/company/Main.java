9e003844d4b5c942ef6bfa4871a8087659eaf931
Gloria Sun <53097343+gloriasun4@users.noreply.github.com>
2021-01-20 14:30:41 -0500

    JTextField

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -7,22 +7,6 @@ import java.awt.*;
 public class Main {
 
     public static void main(String[] args) {
-
-            /* uncomment to get absolutely memed
-            int answer = JOptionPane.showConfirmDialog(null, "Are you freer than a Costco sample?", "Only one right answer.",
-            JOptionPane.YES_NO_CANCEL_OPTION);
-            while (answer != 0) {
-                answer = JOptionPane.showConfirmDialog(null, "Are you freer than a Costco sample?", "Only one right answer.",
-                JOptionPane.YES_NO_CANCEL_OPTION);
-            }
-            String answer = JOptionPane.showInputDialog("What's up?: ");
-            System.out.println("Ok, so " + answer + ". Any askers?");
-            */
-        String[] responses = {"Yeah, ME", "You're mean", "Huh?"};
-        ImageIcon icon = new ImageIcon("egg_smaller.jpg");
-        JOptionPane.showOptionDialog(null, "Any askers?", null,
-                JOptionPane.YES_NO_CANCEL_OPTION, JOptionPane.INFORMATION_MESSAGE, icon, responses, 0);
-
-
+        MyFrame frame = new MyFrame();
     }
 }
--- src/com/company/MyFrame.java
+++ src/com/company/MyFrame.java
@@ -8,49 +8,40 @@ import java.awt.event.ActionListener;
 public class MyFrame extends JFrame implements ActionListener {
 
     JButton button;
-    JLabel label;
+    JTextField textField;
 
     MyFrame() {
 
-        ImageIcon icon = new ImageIcon("egg_smaller.jpg");
-        ImageIcon icon2 = new ImageIcon("mr_small.gif");
+        textField = new JTextField();
+        textField.setPreferredSize(new Dimension(250,40));
+        textField.setFont(new Font("Consolas", Font.PLAIN, 35));
+        textField.setForeground(Color.red);
+        textField.setBackground(Color.black);
+        textField.setCaretColor(Color.white);
+        textField.setText("Username: ");
 
-        button = new JButton();
-        label = new JLabel();
-
-        label.setIcon(icon2);
-        label.setBounds(150,250,150,150);
-        label.setVisible(false);
-
-        button.setBounds(200,200,250,100);
+        button = new JButton("Submit");
         button.addActionListener(this);
-        button.setText("I'm a button. period");
-        button.setFocusable(false);
-        button.setIcon(icon);
-        button.setHorizontalTextPosition(JButton.CENTER);
-        button.setVerticalTextPosition(JButton.BOTTOM);
-        button.setIconTextGap(-10);
-        button.setFont(new Font("Comic Sans", Font.BOLD, 10));
-        button.setForeground(Color.cyan);
-        button.setBackground(Color.lightGray);
-        button.setBorder(BorderFactory.createEtchedBorder());
 
         this.setDefaultCloseOperation(EXIT_ON_CLOSE);
-        this.setLayout(null);
-        this.setSize(500,500);
+        this.setLayout(new FlowLayout());
 
-        this.setVisible(true);
+        //has to be before pack
         this.add(button);
-        this.add(label);
+        this.add(textField);
+
+        this.pack();
+        this.setVisible(true);
 
     }
 
     @Override
     public void actionPerformed(ActionEvent e) {
-        if(e.getSource()== button) {
-            //System.out.println("purr");
-           // button.setEnabled(false);
-            label.setVisible(true);
+        if (e.getSource()==button) {
+            System.out.println("Hello " + textField.getText());
+            button.setEnabled(false);
+            textField.setEditable(false);
         }
     }
 }
+
