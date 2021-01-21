45ec8ee1e3510aeafca734b54f7e0779a4e6e3b3
Gloria Sun <53097343+gloriasun4@users.noreply.github.com>
2021-01-21 15:18:13 -0500

    JFileChooser

--- src/com/company/MyFrame.java
+++ src/com/company/MyFrame.java
@@ -5,78 +5,39 @@ import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.KeyEvent;
+import java.io.File;
 
 public class MyFrame extends JFrame implements ActionListener {
 
-    JMenuBar menuBar;
-    JMenu fileMenu;
-    JMenu editMenu;
-    JMenu helpMenu;
-    JMenuItem loadItem;
-    JMenuItem saveItem;
-    JMenuItem exitItem;
-    ImageIcon loadIcon;
-    ImageIcon saveIcon;
-    ImageIcon exitIcon;
+    JButton button;
 
     MyFrame() {
 
         this.setDefaultCloseOperation(EXIT_ON_CLOSE);
-        this.setSize(500,500);
         this.setLayout(new FlowLayout());
 
-        loadIcon = new ImageIcon("pizza_emoji.png");
-        saveIcon = new ImageIcon("hamburger_emoji.png");
-        exitIcon = new ImageIcon("hot-dog_emoji.png");
+        button = new JButton("Select File");
+        button.addActionListener(this);
 
-        menuBar = new JMenuBar();
-        fileMenu = new JMenu("File");
-        editMenu = new JMenu("Edit");
-        helpMenu = new JMenu("Help");
-
-        loadItem = new JMenuItem("Load");
-        saveItem = new JMenuItem("Save");
-        exitItem = new JMenuItem("Exit");
-
-        loadItem.addActionListener(this);
-        saveItem.addActionListener(this);
-        exitItem.addActionListener(this);
-
-        loadItem.setIcon(loadIcon);
-        saveItem.setIcon(saveIcon);
-        exitItem.setIcon(exitIcon);
-
-        fileMenu.setMnemonic(KeyEvent.VK_F);
-        editMenu.setMnemonic(KeyEvent.VK_E);
-        helpMenu.setMnemonic(KeyEvent.VK_H);
-
-        loadItem.setMnemonic(KeyEvent.VK_L);
-        saveItem.setMnemonic(KeyEvent.VK_S);
-        exitItem.setMnemonic(KeyEvent.VK_E);
-
-        fileMenu.add(loadItem);
-        fileMenu.add(saveItem);
-        fileMenu.add(exitItem);
-
-        menuBar.add(fileMenu);
-        menuBar.add(editMenu);
-        menuBar.add(helpMenu);
-
-        this.setJMenuBar(menuBar);
+        this.add(button);
+        this.pack();
         this.setVisible(true);
 
     }
 
     @Override
     public void actionPerformed(ActionEvent e) {
-        if (e.getSource()==loadItem) {
-            System.out.println("Loading...");
-        }
-        if (e.getSource()==saveItem) {
-            System.out.println("Saving...");
-        }
-        if (e.getSource()==exitItem) {
-            System.exit(0);
+        if(e.getSource() == button) {
+            JFileChooser fileChooser = new JFileChooser();
+
+            fileChooser.setCurrentDirectory(new File("."));
+
+            int response = fileChooser.showOpenDialog(null);
+            //showSaveDialog will replace Open button with Save button
+            if(response==JFileChooser.APPROVE_OPTION) {
+                File file = new File(fileChooser.getSelectedFile().getAbsolutePath());
+                System.out.println(file);
+            }
         }
     }
 }
