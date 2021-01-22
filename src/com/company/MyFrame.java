b242356ab3f1c2052bfdf8cf657502c3f368f01c
Gloria Sun <gloriasun4@gmail.com>
2021-01-22 12:13:43 -0500

    KeyListener, switch()

--- src/com/company/MyFrame.java
+++ src/com/company/MyFrame.java
@@ -5,42 +5,71 @@ import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
 import java.io.File;
 
-public class MyFrame extends JFrame implements ActionListener {
+public class MyFrame extends JFrame implements KeyListener {
 
-    JButton button;
     JLabel label;
+    ImageIcon icon;
 
     MyFrame() {
 
-        this.setDefaultCloseOperation(EXIT_ON_CLOSE);
-        this.setLayout(new FlowLayout());
-
-        button = new JButton("Choose a color!");
-        button.addActionListener(this);
+        this.getContentPane().setBackground(Color.black);
 
         label = new JLabel();
-        label.setBackground(Color.white);
-        label.setOpaque(true);
-        label.setText("This is some text");
-        label.setFont(new Font("SansSerif", Font.BOLD, 50));
+        label.setBounds(200,350, 100, 100);
+        //label.setBackground(Color.red);
+        //label.setOpaque(true);
 
-        this.add(button);
+        icon = new ImageIcon(getClass().getResource("egg_small.jpg"));
+        label.setIcon(icon);
+
         this.add(label);
-        this.pack();
+
+        this.setDefaultCloseOperation(EXIT_ON_CLOSE);
+        this.setSize(500,500);
+        this.setLayout(null);
+        this.addKeyListener(this);
+
+
         this.setVisible(true);
 
     }
 
-    @Override
-    public void actionPerformed(ActionEvent e) {
-        if(e.getSource() == button) {
-            JColorChooser colorChooser = new JColorChooser();
 
-            Color color = JColorChooser.showDialog(null, "Pick a color", Color.black);
-            label.setBackground(color);
+    @Override
+    public void keyTyped(KeyEvent e) {
+        switch(e.getKeyChar()) {
+            case 'w': label.setLocation(label.getX(), label.getY()-5);
+                break;
+            case 'a': label.setLocation(label.getX()-5, label.getY());
+                break;
+            case 's': label.setLocation(label.getX(), label.getY()+5);
+                break;
+            case 'd': label.setLocation(label.getX()+5, label.getY());
+                break;
         }
+    }
+
+    @Override
+    public void keyPressed(KeyEvent e) {
+        switch(e.getKeyCode()) {
+            case 38: label.setLocation(label.getX(), label.getY()-5);
+                break;
+            case 37: label.setLocation(label.getX()-5, label.getY());
+                break;
+            case 40: label.setLocation(label.getX(), label.getY()+5);
+                break;
+            case 39: label.setLocation(label.getX()+5, label.getY());
+                break;
+        }
+    }
+
+    @Override
+    public void keyReleased(KeyEvent e) {
+        System.out.println("You released key char: " + e.getKeyChar());
+        System.out.println("You released key code: " + e.getKeyCode());
     }
 }
 
